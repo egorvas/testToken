fe33d2343862d28e78a7e14bca58a57f45f029ae
Egor Vasilyev <egorvas@gmail.com>
2019-09-11 01:23:07 +0300

    Add jenkinsfile

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,8 @@ module.exports = {
   networks: {
 
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
+     host: process.env.HOST || "127.0.0.1",     // Localhost (default: none)
+     port: process.env.PORT || 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     }
   },
