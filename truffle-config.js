c513052bf2eacde52abd54c01d2a7cf51572d706
Egor Vasilyev <egorvas@gmail.com>
2019-09-11 16:30:15 +0300

    Chamge default port for truffle

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     development: {
      host: process.env.HOST || "127.0.0.1",     // Localhost (default: none)
-     port: process.env.PORT || 7545,            // Standard Ethereum port (default: none)
+     port: process.env.PORT || 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     }
   },
